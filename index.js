050b61850fd63f961a6e1e66f48fa968290a0a6f
Mari-Artho <mari.artho@medieinstitutet.se>
2022-09-21 21:34:36 +0200

    Add blinking subscribe message.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-import 'animate.css';
+//import './animate.css';
+
+
+let cx = document.querySelector("canvas").getContext("2d");
+cx.font = "30px optima";
+cx.fillStyle = "red";
+cx.textAlign = "left";
+cx.fillText("SUBSCRIBE!", 70, 30);
+/*index.html, CANVAS */
+
+let flg=true;
+function blink(){
+if(flg){
+document.querySelector("canvas").style.visibility="visible";
+}else{
+document.querySelector("canvas").style.visibility="hidden";
+}
+flg=!flg;
+setTimeout(blink,777);
+}
+blink();
